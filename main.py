e8b7c65f217f50c564df018ee5d41067da02dff5
nbmadharia <nbmadharia@gmail.com>
2022-02-13 01:32:27 +0530

    initial main

---
+++ main.py
@@ -0,0 +1,24 @@
+# import necessary packages for hand gesture recognition project using Python OpenCV
+
+import cv2
+import numpy as np
+import mediapipe as mp
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+
+
+# initialize mediapipe
+mpHands = mp.solutions.hands
+hands = mpHands.Hands(max_num_hands=1, min_detection_confidence=0.7)
+mpDraw = mp.solutions.drawing_utils
+
+
+
+# Load the gesture recognizer model
+model = load_model('mp_hand_gesture')
+
+# Load class names
+f = open('gesture.names', 'r')
+classNames = f.read().split('\n')
+f.close()
+print(classNames)
